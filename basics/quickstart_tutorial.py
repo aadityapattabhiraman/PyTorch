b80e7e3e2b931e14347b26caf10dd7a092f31504
aadityapattabhiraman <aadityapattabhiraman47@proton.me>
2024-08-16 04:38:46 +0000

    variable name: model

--- basics/quickstart_tutorial.py
+++ basics/quickstart_tutorial.py
@@ -89,7 +89,7 @@ def test(dataloader, model, loss_fn):
 	test_loss /= num_batches 
 	correct /= size 
 	print(f"Test error:\nAccuracy: {(100 * correct):>0.1f}%,",
-		"Avg loss: {test_loss:>8f} \n")
+		f"Avg loss: {test_loss:>8f} \n")
 
 
 epochs = 5
